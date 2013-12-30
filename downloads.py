8ea021c9c89670d1498ee52b1e640e51328706ba
Anna G <hgommers@post.harvard.edu>
2013-12-30 14:58:59 -0800

    Initial commit of the script to scrape the number of downloads for a paper (given its title) from ACM

---
+++ downloads.py
@@ -0,0 +1,50 @@
+# Input: paper title 
+# Output: total number of downloads in the last 6 weeks, in the last 12 months and overall
+
+import urllib2
+from bs4 import BeautifulSoup
+
+def downloads(title):
+    
+    # put the title into proper form to use as a query 
+    query = title.rstrip().lstrip().replace(" ","-");
+
+    # base site 
+    base = "http://dl.acm.org/results.cfm?query=" + query
+    
+    # sketchy things
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent','Mozilla/5.0')]
+    response = opener.open(base)
+    
+    # find the string that contains all of the download data
+    soup = BeautifulSoup(response.read())
+    data = soup.find_all("strong")
+    for i in data:
+        result = str(i).find("Bibliometrics")
+        if result > 0:
+            scramble =  i.parent
+    
+    # extract the three download numbers  
+    string_scramble = str(scramble).split('/strong>:')[1].replace('\n',' ').split(',')
+
+    downloads = []
+    for i in string_scramble:
+        downloads.add(i.split(':')[1])
+
+    return downloads 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
