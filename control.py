9050ca0765bec3fa74ac5345f196c0561985c948
mnadler <mnadler@umich.edu>
2013-12-31 05:50:02 -0500

    ready for social feat

--- control.py
+++ control.py
@@ -4,26 +4,30 @@ from scholar import txt
 
 def main(query):
 
+	article_list = []
 	
-	csv_data = csv(query, author='',  count=1)#, header=True)
-	print csv_data
+	csv_data = csv(query, author='',  count=20)#, header=True)
+
 	for line in csv_data:
-#		print line
 		line = line.split('|')
+
 		title = line[0]
 		authors = line[7].split(', ')
 		venue = line[8].rstrip()
 		venue = venue.lstrip()
-		print venue
 		version_urls = [line[1]]
-		if False and len(line) >3 and int(line[3]) > 0: 
+
+		if len(line) >3 and int(line[3]) > 0: 
 			version_data = url_get(line[5], author='', count=20)
 			for subline in version_data:
 				subline = subline.split('|')
 				version_urls.append(subline[1])
+
+		article_list.append((title, authors, version_urls, venue))
+
+	print article_list
 				
-#		print version_urls
-		afds
+		
 
 
 if __name__ == '__main__':
