023e7c1240c180300dbfc5413705efd81fc80c82
mnadler <mnadler@umich.edu>
2013-12-31 07:07:04 -0500

    to n

--- control.py
+++ control.py
@@ -2,8 +2,13 @@ from scholar import csv
 from scholar import url_get
 from scholar import txt
 import social_trends
+import time
+import downloads
+import json
 
 def main(query):
+
+	json_output = []
 
 	article_list = []
 	
@@ -26,11 +31,38 @@ def main(query):
 
 		article_list.append((authors, title, version_urls, venue))
 
-	for paper in article_list:
-		tw = social_trends.SocialTrends(authors, title, version_urls)
-				
-		
+	toSort = []
 
+	for paper in article_list:
+
+		tw = social_trends.SocialTrends(paper[0], paper[1], paper[2])
+		(score, tweets) = tw.paper_tweets()
+		recent_downloads = downloads.downloads(paper[1])	
+	
+		if score > 0 or recent_downloads[0] > 0:	
+			json_paper = dict()
+			json_paper['title'] = paper[1]
+			json_paper['authors'] = paper[0]
+			json_paper['urls'] = paper[2]
+			json_paper['venue'] = paper[3]
+			json_paper['tweets'] = []
+
+			for (tid, tweet) in tweets:
+				print tweet
+				json_tweet = dict()
+				json_tweet['author'] = tweet['user']['screen_name']
+				json_tweet['content'] = tweet['text']
+				json_tweet['url'] = "http://twitter.com/"+tweet['screen_name']+"/status/"+tweet['id']
+				json_paper['tweets'].append(json_tweet)
+
+				
+			toSort.append((score, recent_downloads[0], json_paper))							
+		time.sleep(2)
+
+	toJson= [y[2] for y in sorted(toSort, key=lambda x : x[0]*10000+x[1])]
+
+	return json.dump(toJson)
+	
 
 if __name__ == '__main__':
-	main('bloom filter')
+	main('Retweeting Fukushima')
