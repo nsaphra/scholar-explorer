09d24f535965b0791683ec817ece766e74703b7c
mnadler <mnadler@umich.edu>
2013-12-31 05:33:14 -0500

    ready to interface with n

--- control.py
+++ control.py
@@ -3,19 +3,27 @@ from scholar import url_get
 from scholar import txt
 
 def main(query):
+
 	
-	csv_data = csv(query, author='',  count=20)#, header=True)
+	csv_data = csv(query, author='',  count=1)#, header=True)
+	print csv_data
 	for line in csv_data:
-		print line
+#		print line
 		line = line.split('|')
-		if int(line[3]) > 0: 
+		title = line[0]
+		authors = line[7].split(', ')
+		venue = line[8].rstrip()
+		venue = venue.lstrip()
+		print venue
+		version_urls = [line[1]]
+		if False and len(line) >3 and int(line[3]) > 0: 
 			version_data = url_get(line[5], author='', count=20)
-			print 'list start'
 			for subline in version_data:
 				subline = subline.split('|')
-				print subline[1]
-			print 'list end'
+				version_urls.append(subline[1])
 				
+#		print version_urls
+		afds
 
 
 if __name__ == '__main__':
