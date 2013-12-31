fd29e45192eb5df4e03656730d74415c38e58bf9
N. Saphra <nsaphra@gmail.com>
2013-12-31 10:17:59 +0200

    removed some dead code

--- social_trends.py
+++ social_trends.py
@@ -1,23 +1,8 @@
 import time, random
 import sys
-#sys.path.append("GoogleScraper-master")
-#import GoogleScraper as gs
 from twython import Twython, TwythonError
-#import praw
 import KEYS
 import pickle
-
-#TINYURL_SHORTEN = "http://tinyurl.com/api-create.php?url="
-#OWLY_SHORTEN = "http://ow.ly/api/1.1/url/shorten?apiKey=$ACCESS&longUrl="
-#bitly = bitly_api.Connection(access_token=$ACCESS)
-    
-#def get_short_urls(url):
-#    shorts = []
-#    shorts += [bitly.shorten(url)]
-#    shorts += [urllib2.urlopen(TINYURL_SHORTEN+url).read()]
-#    shorts += [Googl.shorten(url).short_url]
-#    shorts += [urllib2.urlopen(OWLY_SHORTEN+url)]
-#    return shorts
 
 PICKLE_FILE = "social_trends.pickle"
 social_cache = pickle.load(open(PICKLE_FILE, "rb"))
@@ -29,13 +14,11 @@ class SocialTrends:
         
         if (author, title) in social_cache:
             self.twitter_results = social_cache[(author, title)]['twitter']
-            #self.reddit_results = social_cache[(author, title)]['reddit']
             return
 
         social_cache[(self.author, self.title)] = {}
         (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) = KEYS.keys()
         self.tw = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-        #self.rd = praw.Reddit(user_agent="scholar-explorer")
         self.urls = urls
         self.twitter_results = ""
         self.reddit_results = ""
@@ -43,7 +26,7 @@ class SocialTrends:
     def paper_tweets(self):
         if self.twitter_results:
             return self.twitter_results
-            
+
         #allnames = author.split() # possible author names
         #lastname = allnames[len(allnames)-1]
         tweets = []
@@ -57,14 +40,8 @@ class SocialTrends:
 
         # possible urls
         for url in self.urls:
-            #short_urls = get_short_urls(scholar_url)
-            #for short_url in short_urls:
-            #    results += gs.scrape("site:twitter.com " + short_url)
-            #    time.sleep(random.uniform(2, 5))
             results = self.tw.search(q=url, count=100)
             tweets += [status['id'] for status in results['statuses']]
-            #for tweet in results['statuses']:
-            #    print(tweet)
 
         #TODO better scoring system please
         trend_score = len(tweets)
@@ -75,17 +52,3 @@ class SocialTrends:
         social_cache[(self.author, self.title)]['twitter'] = self.twitter_results
         pickle.dump(social_cache, open(PICKLE_FILE,'wb'))
         return self.twitter_results
-
-    #TODO finish implementing
-    def paper_reddits(self):
-        if self.reddit_results:
-            return self.reddit_results
-            
-        reddits = []
-        for url in self.urls:
-            results = self.rd.search(url)
-            for result in results:
-                new_reddit = {}
-                #TODO
-
-        self.reddit_results = (trend_score, top_reddits)
